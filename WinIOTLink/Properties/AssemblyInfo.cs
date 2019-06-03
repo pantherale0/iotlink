1f9bed3e41018e2439f0c27f0e73506f2f8f035f
Alexandre Leites <alexslx@live.com>
2019-06-02 22:15:23 -0300

    Version information

--- WinIOTLink/Properties/AssemblyInfo.cs
+++ WinIOTLink/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinIOTLink")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Win IOT Link")]
+[assembly: AssemblyDescription("Service to provide Internet Of Things (IOT) integration with Windows using MQTT.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("alexslx.com")]
 [assembly: AssemblyProduct("WinIOTLink")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Alexandre Leites 2019")]
+[assembly: AssemblyTrademark("Copyright © Alexandre Leites 2019")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
