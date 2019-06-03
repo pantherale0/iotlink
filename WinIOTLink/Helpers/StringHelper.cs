247256db3c5882a0ffb17d74c1249de727f3686e
Alexandre Leites <alexslx@live.com>
2019-06-03 05:44:44 -0300

    Using MS naming convention

--- WinIOTLink/Helpers/StringHelper.cs
+++ WinIOTLink/Helpers/StringHelper.cs
@@ -141,22 +141,47 @@ namespace WinIOTLink.Helpers
         }
 
         /// <summary>
-        /// Convert PascalCase to kebab-case
+        /// Convert PascalCase to kebab-case.
+        /// Using https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/capitalization-conventions
         /// </summary>
         /// <param name="value">String using PascalCase</param>
         /// <returns>String using kebab-case</returns>
-        public static string PascalToKebabCase(this string value)
+        public static string PascalToKebabCase(this string source)
         {
-            if (string.IsNullOrEmpty(value))
-                return value;
+            if (source is null)
+                return null;
 
-            return Regex.Replace(
-                value,
-                "(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z])",
-                "-$1",
-                RegexOptions.Compiled)
-                .Trim()
-                .ToLower();
+            if (source.Length == 0)
+                return string.Empty;
+
+            StringBuilder builder = new StringBuilder();
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (char.IsLower(source[i])) // if current char is already lowercase
+                {
+                    builder.Append(source[i]);
+                }
+                else if (i == 0) // if current char is the first char
+                {
+                    builder.Append(char.ToLower(source[i]));
+                }
+                else if (char.IsLower(source[i - 1])) // if current char is upper and previous char is lower
+                {
+                    builder.Append("-");
+                    builder.Append(char.ToLower(source[i]));
+                }
+                else if (i + 1 == source.Length || char.IsUpper(source[i + 1])) // if current char is upper and next char doesn't exist or is upper
+                {
+                    builder.Append(char.ToLower(source[i]));
+                }
+                else // if current char is upper and next char is lower
+                {
+                    builder.Append("-");
+                    builder.Append(char.ToLower(source[i]));
+                }
+            }
+            return builder.ToString();
         }
     }
 }
